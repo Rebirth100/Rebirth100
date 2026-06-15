00e2a9816b990e3b5fdcfebb05e62024cf3a3d12
Rebirth100 <3518231079@qq.com>
2026-06-16 00:19:45 +0800

    Site updated: 2026-06-16 00:19:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/06/15/hello-world/"];function toRandomPost(){
+var posts=["2026/06/15/hello-world/","2026/06/15/2026/2026.6/测试/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
