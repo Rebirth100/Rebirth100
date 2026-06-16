522d6ec8b4412fef9eeb1140b08722a6ebc156c4
Rebirth100 <3518231079@qq.com>
2026-06-16 15:05:57 +0800

    Site updated: 2026-06-16 15:05:57

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["uncategorized/undefined/2026/06/15/hello-world/","uncategorized/undefined/2026/06/16/blog/","uncategorized/undefined/2026/06/16/2026年6月16日14-03-31/","uncategorized/undefined/2026/06/15/2026/2026.6/测试/"];function toRandomPost(){
+var posts=["Rivalries/undefined/2026/06/16/2026年6月16日14-03-31/","uncategorized/undefined/2026/06/15/hello-world/","uncategorized/undefined/2026/06/16/blog/","uncategorized/undefined/2026/06/15/2026/2026.6/测试/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
