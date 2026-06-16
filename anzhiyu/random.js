fedb44f5b8168f0caf3c10c2a5f4d3eaaec2f392
Rebirth100 <3518231079@qq.com>
2026-06-16 11:52:59 +0800

    Site updated: 2026-06-16 11:52:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/06/15/categories/index/","2026/06/15/hello-world/","2026/06/15/2026/2026.6/测试/"];function toRandomPost(){
+var posts=["uncategorized/undefined/2026/06/16/blog/","uncategorized/undefined/2026/06/15/2026/2026.6/测试/","uncategorized/undefined/2026/06/15/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
